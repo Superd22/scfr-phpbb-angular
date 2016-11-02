50b16202c27c8914fce27ddd326b9e18ff4a00f7
fro <fro@moonda.com>
2016-11-02 18:04:00 +0100

    Few tweaks

--- src/app/service/phpbb-api.service.ts
+++ src/app/service/phpbb-api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
-import { Observable } from "rxjs/Rx";
+import { Observable } from 'rxjs/Rx';
 import { PhpbbTemplateResponse } from '../model/phpbb-template-response'
-import { IndexResponse } from "../model/IndexResponse";
+import { IndexResponse } from '../model/IndexResponse';
 import { UnreadResponse } from '../model/Search/UnreadReponse';
 import { UcpResponse } from '../model/UcpResponse';
 
@@ -16,84 +16,79 @@ export class PhpbbApiService {
     private sid: string|null;
     constructor(private http: Http){}
 
-    public buildParameters(arrayOfParam?: any[][]): string{
+    public buildParameters(arrayOfParam?: {}): string{
         let urlParam = new URLSearchParams();
-        if(arrayOfParam)
-        for(let param of arrayOfParam){
-            urlParam.append(param[0], param[1])
+        for(let paramKey in arrayOfParam){
+            if (arrayOfParam.hasOwnProperty(paramKey)) {
+                urlParam.append(paramKey, arrayOfParam[paramKey]);
+            }
         }
         urlParam.append('scfr_json_callback', 'true');
         urlParam.append('sid', this.sid);
-        return urlParam.toString()
+        return urlParam.toString();
     }
 
     public registerSid(sid){
         this.sid = sid;
     }
 
-    public getPage(page, queries?:any[][]):Observable<PhpbbTemplateResponse.DefaultResponse> {
+    public getPage(page, queries?: {}):Observable<PhpbbTemplateResponse.DefaultResponse> {
         return this.http.get(`${baseUrl}${page}`, {search: this.buildParameters(queries)} )
         .map((res:Response) => res.json())
         .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
 
     public getIndex():Observable<IndexResponse.IndexRoot>{
-        return this.getPage("index.php");
+        return this.getPage('index.php');
     }
 
     public getMessage(){
-        return this.getPage("ucp.php", [ ['i', 'pm'], ['folder', 'inbox'] ]);
+        return this.getPage('ucp.php', {i: 'pm', folder: 'inbox'});
     }
 
     public getForumById(forum: number):Observable<PhpbbTemplateResponse.DefaultResponse> {
-        return this.getPage("viewforum.php", [ ['f', forum] ] );
+        return this.getPage('viewforum.php', {f: forum});
     }
 
     public getTopicById(topic: number):Observable<PhpbbTemplateResponse.DefaultResponse> {
-        return this.getPage("viewtopic.php", [ ['t', topic ] ] );
+        return this.getPage('viewtopic.php', {t: topic});
     }
 
     public getPrivateMessageList():Observable<UcpResponse.Messagerow[]>{
-        let params = [
-            ['i', 'pm'],
-            ['folder', 'inbox']
-        ];
+        let params = {i: 'pm', folder: 'inbox'};
         return this.http.get(`${baseUrl}ucp.php`, {search: this.buildParameters(params)} )
             .map((res:Response) => res.json()['@template'].messagerow)
             .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
 
     public getUnreadTopicList():Observable<UnreadResponse.Searchresult[]>{
-        let params = [
-            ['search_id', 'unreadposts'],
-        ];
+        let params = {search_id: 'unreadposts'} ;
         return this.http.get(`${baseUrl}search.php`, {search: this.buildParameters(params)})
             .map((res:Response) => res.json()['@template'].searchresults)
             .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
 
-
-    //LOGIN
+    // LOGIN
     public authenticate(username, password, sid, remember): Observable<IndexResponse.IndexRoot>{
         let headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded');
         let redirect = `index.php?${callback}`;
-        let params = [
-            ['username', username],
-            ['password', password],
-            ['sid', sid],
-            ['login', 'Login'],
-            ['redirect', redirect],
-            ['mode','login']
-        ];
-        if(remember) params.push(['autologin', 'true']);
+        let params: any = {
+            username: username,
+            password: password,
+            sid: sid,
+            login: 'login',
+            redirect: redirect,
+            mode: 'login'
+        };
+        if(remember) params.autologin = 'true';
         return this.http.post(`${baseUrl}ucp.php`, this.buildParameters(params), {headers: headers})
             .map((res:Response) => res.json()['@template'])
             .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
 
     public getAuthentication(): Observable<IndexResponse.Template>{
-        return this.http.get(`${baseUrl}`, {search: this.buildParameters([])})
+        return this.http.get(`${baseUrl}`, {search: this.buildParameters()})
             .map((res:Response) => res.json()['@template'])
             .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
