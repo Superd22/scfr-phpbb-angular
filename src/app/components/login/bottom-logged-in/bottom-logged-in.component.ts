cf67f8b347da2075cf4a909413ff13925cf28dbd
Superd22 <superd001@gmail.com>
2017-06-12 14:57:51 +0200

    Miscs

---
+++ src/app/components/login/bottom-logged-in/bottom-logged-in.component.ts
@@ -0,0 +1,20 @@
+import { StateTranslate } from './../../../services/state-translate.service';
+import { LoginService } from './../../../services/login.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-logged-in',
+  templateUrl: './bottom-logged-in.component.html',
+  styleUrls: ['./bottom-logged-in.component.scss']
+})
+export class BottomLoggedInComponent implements OnInit {
+
+  public currentTpl;
+
+  constructor(public LoginService: LoginService, private stateT: StateTranslate) { }
+
+  ngOnInit() { 
+    this.stateT.latestTemplateData.subscribe( (tpl) => this.currentTpl = tpl )
+  }
+
+}
