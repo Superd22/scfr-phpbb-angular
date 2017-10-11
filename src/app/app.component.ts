345918845edcf477b0f1a04f4a3d3fda0ae6e5e0
Superd22 <superd001@gmail.com>
2017-10-11 23:27:26 +0200

    Fix(header) fix aot issue with AppComponent trying to inject a tpl it doesn't have (tsk tsk tsk c/c @Kable)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
+import { IPhpbbTemplate } from './interfaces/phpbb/phpbb-tpl';
+import { StateTranslate } from './services/state-translate.service';
+import { PhpbbComponent } from './components/phpbb/phpbb-component.component';
 import { ExtraModuleInjector } from './decorators/ExtraModuleInjector';
 import { SCFRLocalStorage } from './decorators/LocalStorage.decorator';
 import { LayoutService } from './material/services/layout-service.service';
 import { Component, OnInit } from '@angular/core';
-import {NavigationService} from 'app/services/navigation.service';
+import { NavigationService } from 'app/services/navigation.service';
 
 @Component({
     selector: 'scfr-phpbb',
@@ -14,14 +17,16 @@ export class AppComponent implements OnInit {
 
     public title = 'SCFR PHPBB APP';
     public mode = 'side';
-    @SCFRLocalStorage('navbar:toggle')
-    public toggle: boolean;
+    public tpl: IPhpbbTemplate;
 
     constructor(
         private layout: LayoutService,
         private extra: ExtraModuleInjector,
-        public navigation: NavigationService
-    ) {}
+        public navigation: NavigationService,
+        public state: StateTranslate
+    ) {
+        this.state.latestTemplateData.subscribe((tpl) => this.tpl = tpl);
+    }
 
     ngOnInit() {
         this.layout.gt_md.subscribe((gt_md) => {
