4c90e620faea85c68a936a84e78313f93a8daf79
Superd22 <superd001@gmail.com>
2017-11-05 07:31:34 +0100

    Fix(header) wrong calls on prod

--- src/app/header/services/global-header.service.ts
+++ src/app/header/services/global-header.service.ts
@@ -36,7 +36,7 @@ export class GlobalHeaderService {
     if (force) fetch = true;
 
     if (fetch) {
-      this.http.get(environment.baseForumUrl + "/wp-json/HeaderBar/Full/").subscribe((res) => {
+      this.http.get(environment.baseForumUrl + "../wp-json/HeaderBar/Full/").subscribe((res) => {
         this._headerDataCache.next(res.json());
       });
     }
@@ -63,7 +63,7 @@ export class GlobalHeaderService {
    * Fetches data from the forum
    */
   public fetchForumData() {
-    this.http.get(environment.baseForumUrl + "/Forum/?scfr_json_callback=true", { withCredentials: true }).subscribe((res) => {
+    this.http.get(environment.baseForumUrl + "?scfr_json_callback=true", { withCredentials: true }).subscribe((res) => {
       let tpl = res.json()['@template'];
       this.setForumData(tpl);
     });
