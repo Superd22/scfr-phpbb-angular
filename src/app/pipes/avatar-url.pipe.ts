8ca977ec60b670506521af0a25ec3773f0c76c4c
Superd22 <superd001@gmail.com>
2017-11-06 07:48:32 +0100

    Fix bug avatar

--- src/app/pipes/avatar-url.pipe.ts
+++ src/app/pipes/avatar-url.pipe.ts
@@ -6,12 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SCFRAvatarURL implements PipeTransform {
 
     transform(value: string): string {
+        if(!value) return "https://starcitizen.fr/Forum/assets/images/default-avatar.jpg";
+
         if (value.indexOf("starcitizen.fr/Forum/download/file.php?") > -1) {
             value = value.replace("starcitizen.fr/Forum/download/", "starcitizen.fr/forum-api/download/");
         }
 
-
-        if(!value) return "https://starcitizen.fr/Forum/assets/images/default-avatar.jpg";
         return value;
     }
 }
