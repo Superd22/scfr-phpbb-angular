5af1ca1a65dd047f131e4dae03bc2130ace6266c
Superd22 <superd001@gmail.com>
2017-11-06 07:38:32 +0100

    Fix(headerbar) fix broken link sur boutton forum

--- src/app/header/enums/main-links.const.ts
+++ src/app/header/enums/main-links.const.ts
@@ -1,9 +1,9 @@
 import { IMainNavLink } from './../interfaces/main-nav-link.interface';
 export const mainLinks: IMainNavLink[] = [
     { name: "News", id: 0, target: "/", menuType: "full"},
-    { name: "Forum", id: 1, target: "Forum/", menuType: "none" },
+    { name: "Forum", id: 1, target: "https://starcitizen.fr/Forum/", menuType: "none" },
     { name: "Star Citizen", id: 2, target: "star-citizen/", menuType: "full" },
     { name: "Communauté", id: 3, target: "communityhub/", menuType: "full" },
     { name: "Outils", id: 4, target: "star-citizen", menuType: "small" },
-    { name: "JULIET", id: 5, target: "https://www.starcitizen.fr/Flotte/", menuType: "none", julietOnly: true },
+    { name: "JULIET", id: 5, target: "https://juliet.starcitizen.fr/", menuType: "none", julietOnly: true },
 ];
