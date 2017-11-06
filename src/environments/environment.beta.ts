6537bead5ad1b06b3076a3f0e5a5a1232db8d221
Superd22 <superd001@gmail.com>
2017-11-06 10:05:30 +0100

    Fix(recaptcha) fix captcha invisible a l'inscription

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     baseForumUrl: 'https://beta.starcitizen.fr/forum-api/',
-    reCaptchaKey: "",
+    reCaptchaKey: "6Lf3qeUSAAAAAHTD2QRwimj7ZuJQWrPglz6sriI4",
     websocket: 'wss://beta.starcitizen.fr:8080',
     baseHref: "/Forum/",
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   baseForumUrl: 'https://starcitizen.fr/forum-api/',
-  reCaptchaKey: "",
+  reCaptchaKey: "6LfVmeUSAAAAACv_hIeVFhNxf4LPYr03GY8TnphL",
   websocket: 'wss://starcitizen.fr:8080',
   baseHref: "/Forum/",
 };
